56cf5086613742ac2ab7b5e114239ad820305f0f
umberto palazzo <upalazzo93@gmail.com>
2020-01-29 22:45:10 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
+  has_many :genres, through: :song
+  has_many :notes, through: :song
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :genre
+  belongs_to :artist 
+  has_many :notes
+  accepts_nested_attributes_for :notes
+  accepts_nested_attributes_for :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+
+  def artist_name 
+    self.artist ? self.artist.name : nil 
+  end
 end
